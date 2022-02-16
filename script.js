dea42009fafec6177354420e8ac980ed19ecb6a8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-16 18:54:58 -0300

    Clicar duas vezes em um item, faz com que ele seja riscado

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttonAdd = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 
 function addTasks() {
-  let task = document.createElement('li');
+  const task = document.createElement('li');
   task.innerHTML = input.value;
   input.value = '';
   listTasks.appendChild(task);
@@ -15,8 +15,17 @@ function selectGray(event) {
   for (let index = 0; index < tasks.length; index += 1) {
     if (tasks[index].style.backgroundColor === 'gray') {
       tasks[index].style.backgroundColor = 'white';
-    } 
+    }
   }
   event.target.style.backgroundColor = 'gray';
 }
 listTasks.addEventListener('click', selectGray);
+
+function doubleClick(event) {
+  if (event.target.className === '') {
+    event.target.classList.add('completed');
+  } else {
+    event.target.classList.remove('completed');
+  }
+}
+listTasks.addEventListener('dblclick', doubleClick);
