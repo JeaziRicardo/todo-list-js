3306d9149ac8dd2322830411e5e2c82915b5aa40
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-17 17:39:01 -0300

    Adciona dois botoes que permita mover o item selecionado para cima ou para baixo na lista de tarefas

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const clearButton = document.querySelector('#apaga-tudo');
 const completedButton = document.querySelector('#remover-finalizados');
 const saveButton = document.querySelector('#salvar-tarefas');
 const list = document.getElementsByTagName('ol')[0];
+const buttonUp = document.querySelector('#mover-cima');
+const buttonDown = document.querySelector('#mover-baixo');
 
 function addTasks() {
   const task = document.createElement('li');
@@ -61,4 +63,26 @@ saveButton.addEventListener('click', saveList);
 window.onload = () => {
   const storageSave = localStorage.getItem('listTasks');
   list.innerHTML = storageSave;
+};
+
+// Usei a função 'insertBefore' para fazer às funções upButton e downButton.
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Node/insertBefore
+function upButton() {
+  const tasks = document.querySelectorAll('li');
+  for (let index = 0; index < tasks.length; index += 1) {
+    if (tasks[index] !== tasks[0] && tasks[index].style.backgroundColor === 'gray') {
+      tasks[index].parentNode.insertBefore(tasks[index], tasks[index].previousElementSibling);
+    }
+  }
 }
+buttonUp.addEventListener('click', upButton);
+
+function downButton() {
+  const tasks = document.querySelectorAll('li');
+  for (let index = 0; index < tasks.length; index += 1) {
+    if (tasks[index] !== tasks[tasks.length - 1] && tasks[index].style.backgroundColor === 'gray') {
+      tasks[index].parentNode.insertBefore(tasks[index].nextElementSibling, tasks[index]);
+    }
+  }
+}
+buttonDown.addEventListener('click', downButton);
